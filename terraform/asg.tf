3d3eb056b6ffc53139d4e71b01612cbfa80c78a9
KetuCoder <ambureketan@gmail.com>
2025-12-05 10:06:03 +0530

    update

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -1,27 +1,3 @@
-# IAM Role for EC2
-resource "aws_iam_role" "ec2_role" {
-  name = "ec2-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action    = "sts:AssumeRole",
-      Effect    = "Allow",
-      Principal = { Service = "ec2.amazonaws.com" }
-    }]
-  })
-}
-
-# Attach SSM policy
-resource "aws_iam_role_policy_attachment" "ssm_attach" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2-profile"
-  role = aws_iam_role.ec2_role.name
-}
-
 # Launch Template
 resource "aws_launch_template" "app_lt" {
   name          = "app-lt"
